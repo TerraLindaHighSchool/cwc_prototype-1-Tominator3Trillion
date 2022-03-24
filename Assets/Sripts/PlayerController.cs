4fd9185a9542b4ced28b2f15aaa4fd511014ce72
Tominator3Trillion <89489218+Tominator3Trillion@users.noreply.github.com>
2022-03-24 15:33:22 -0700

    Unit 1 complete

--- Assets/Sripts/PlayerController.cs
+++ Assets/Sripts/PlayerController.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float turnSpeed = 20f;
+    public float speed = 20f;
 
-    // Update is called once per frame
+
+    public GameObject[] tires = new GameObject[4];
+
     void Update()
     {
-        //move the vehichle
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        //get input from horizontal and vertical axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        //rotate tires
+        foreach(GameObject tire in tires)
+        {
+            tire.transform.Rotate(Vector3.right * verticalInput * -speed * 10* Time.deltaTime);
+
+            //set rotation on the y axis to horizontal input times turn speed
+            tire.transform.rotation = Quaternion.Euler(tire.transform.rotation.eulerAngles.x, horizontalInput * turnSpeed + transform.rotation.eulerAngles.y, tire.transform.rotation.eulerAngles.z);
+        }
+
+
+        //move forward or back
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+
+        //rotate left or right
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
