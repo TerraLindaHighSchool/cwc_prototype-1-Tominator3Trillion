0b520804b4cae9bd053df7f0937e47e06ada90fa
Tominator3Trillion <89489218+Tominator3Trillion@users.noreply.github.com>
2022-05-31 15:44:15 -0700

    Enemy and some sound

---
+++ Assets/FighterEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterEnemy : MonoBehaviour
+{
+
+    public GameObject target;
+    public float speed = 10f;
+    public float fireRate = 0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lerp rotation towards target
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), Time.deltaTime * speed);
+        //move forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+}
